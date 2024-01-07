bdf5fdd3e9a652d3ae5070eb5155b3aa94784f11
ashishsarwal <ashish.sarwal@gmail.com>
2024-01-07 16:52:32 +0530

    Images Saved to Path
    Saving Images to Path.

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000; //80; -- Older Port For Server
+const bodyParser = require('body-parser');
+
 
 const topic = require('./routes/topic');
 const blog = require('./routes/blog');
 const topic_blog = require('./routes/topic_blog');
+const blogs_by_topic = require('./routes/blogs_by_topic');
+
+// Express 4.0
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+
 
 app.use(function (req, res, next) {
 
@@ -27,9 +35,11 @@ app.use(function (req, res, next) {
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 app.use('/topic', topic);
 app.use('/blog', blog);
 app.use('/topic_blog', topic_blog);
+app.use('/blogs_by_topic',blogs_by_topic);
 
 app.listen(port, ()=>{
     console.log('listening on port 3000...');
