cc2bd5d1e30e65a5e663206f90968f1b00595943
ashishsarwal <ashish.sarwal@gmail.com>
2024-01-10 22:07:44 +0530

    Updated Acceptance DB

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
 var mysql = require('mysql2');
 
- var con = mysql.createConnection({
-     host: 'localhost',
-     user: 'root',
-     password: 'Groot@3022!!',
-     database: 'thoughtevolution',
-     multipleStatements: true
- });
+//  var con = mysql.createConnection({
+//      host: 'localhost',
+//      user: 'root',
+//      password: 'Groot@3022!!',
+//      database: 'thoughtevolution',
+//      multipleStatements: true
+//  });
 
-// var con = mysql.createConnection({
-//    host: '184.168.112.45',
-//    user: 'asarwal',
-//    password: 'Baha@3022!!',
-//    database: 'building_management',
-//    multipleStatements: true
-// });
+var con = mysql.createConnection({
+   host: '184.168.112.45',
+   user: 'asarwal',
+   password: 'Baha@3022!!',
+   database: 'building_management',
+   multipleStatements: true
+});
 module.exports = con;
