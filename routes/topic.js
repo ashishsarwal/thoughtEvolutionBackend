8a07c70408a12930c6e7e4e97ef22ecbfbf6da7e
ashishsarwal <ashish.sarwal@gmail.com>
2024-02-04 15:22:46 +0530

    Bug Fix Connection Pool

--- routes/topic.js
+++ routes/topic.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-const con  = require('../config/connection');
+const pool  = require('../config/connection');
 
 router.get('/', (req,res) => {
 
